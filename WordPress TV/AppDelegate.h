510f0c384638005b4ca9e25073c1e3b2e29929e6
Amit Sharma <amitsharma@Amits-MacBook-Pro.local>
2015-11-22 11:20:28 -0600

    inital commmit

---
+++ WordPress TV/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WordPress TV
+//
+//  Created by Amit Sharma on 11/22/15.
+//  Copyright © 2015 WPTV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WordPress TV/WTTemplateSophieVC.m
@@ -0,0 +1,37 @@
+//
+//  WTTemplateSophieVC.m
+//  WordPress TV
+//
+//  Created by Amit Sharma on 11/22/15.
+//  Copyright © 2015 WPTV. All rights reserved.
+//
+
+#import "WTTemplateSophieVC.h"
+
+@interface WTTemplateSophieVC ()
+
+@end
+
+@implementation WTTemplateSophieVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
